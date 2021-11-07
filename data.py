836c2ca97fd01e1d4c67113e386509a4067f6e9c
edn0 <corentin.edno.blondiau@gmail.com>
2021-11-07 15:16:56 +0100

    adding track list and extra file to hold unnescessary data

---
+++ data.py
@@ -0,0 +1,15 @@
+# This will be used to store data that would take unnecessary space on the main app
+
+tracks = [
+    "Mettet, Belgium", #0
+    "Trois-Rivières, Canada", #1
+    "Lydden Hill, England", #2
+    "Silverstone, England", #3
+    "Lohéac, France", #4
+    "Hell, Norway", #5
+    "Montalegre, Portugal", #6
+    "Killarney International Raceway, South Africa", #7
+    "Circuit de Barcelona-Catalunya, Spain", #8
+    "Höljes, Sweden", #9
+    "Yas Marina Circuit, Abu Dhabi" #10
+    ]
